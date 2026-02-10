6ed653f68a578d1b65a71d2174cd1b80e254b78c
MoiraCordier <moiracordier@orange.fr>
2026-02-10 20:57:05 +0100

    feat(rules): add rules for conventionnal commit

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -1,1 +1,16 @@
-module.exports = { extends: ["@commitlint/config-conventional"] };
+module.exports = {
+    extends: ["@commitlint/config-conventional"],
+    rules: {
+        // Type obligatoire (feat, fix, etc.)
+        "type-empty": [2, "never"],
+
+        // Scope obligatoire, mais libre
+        "scope-empty": [2, "never"],
+
+        // Sujet obligatoire
+        "subject-empty": [2, "never"],
+
+        // Longueur maximale du header
+        "header-max-length": [2, "always", 100],
+    },
+};
