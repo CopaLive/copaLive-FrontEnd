8db3e7eef850240674f8e2d90a4f5d4fa6737ffa
Jarod <jarod78972@gmail.com>
2026-02-10 18:31:53 +0100

    feat: adds routing and base layout

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,27 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import { Outlet } from 'react-router-dom'
+import { createBrowserRouter } from 'react-router-dom'
+import { routes } from './route'
+import { RouterProvider } from 'react-router-dom'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-  </StrictMode>,
-)
+function main() {
+  
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <Outlet />,
+      children: [routes]
+    },
+  ])
+
+  
+  createRoot(document.getElementById('root')!).render(
+    <StrictMode>
+      <RouterProvider router={router} />
+    </StrictMode>,
+  )
+}
+
+void main();
---
+++ src/route.ts
@@ -0,0 +1,13 @@
+import { lazy } from "react";
+import type { RouteObject } from "react-router-dom";
+
+export const routes: RouteObject = {
+  path: "/",
+  Component: lazy(() => import("./pages/Layout")),
+  children: [
+    {
+      index: true,
+      Component: lazy(() => import("./pages/GamesList")),
+    }
+  ]
+}
